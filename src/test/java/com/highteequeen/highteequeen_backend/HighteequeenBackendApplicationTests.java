4b512c3947c951e07fb0315aa7de86f03d0f5744
TinUIT <20522017@gm.uit.edu.vn>
2023-03-25 20:58:33 +0700

    first commit

---
+++ src/test/java/com/highteequeen/highteequeen_backend/HighteequeenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highteequeen.highteequeen_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighteequeenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
