e8f6dfd0ef98aa24dfd71f713396964b1c3e7ff1
TinUIT <20522017@gm.uit.edu.vn>
2023-05-26 14:27:14 +0700

    fix_cors

--- src/main/java/com/highteequeen/highteequeen_backend/controllers/AuthController.java
+++ src/main/java/com/highteequeen/highteequeen_backend/controllers/AuthController.java
@@ -6,13 +6,12 @@ import com.highteequeen.highteequeen_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
