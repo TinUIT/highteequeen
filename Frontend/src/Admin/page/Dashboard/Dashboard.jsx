b59ab9f4b10b4e8dd427909d300985476501ff18
Quý <20521818@gm.uit.edu.vn>
2023-12-30 16:20:25 +0700

    update dashboard

--- Frontend/src/Admin/page/Dashboard/Dashboard.jsx
+++ Frontend/src/Admin/page/Dashboard/Dashboard.jsx
@@ -1,15 +1,15 @@
 import './Dashboard.css'
 import MainDash from '../../component/MainDash/MainDash';
-
 import Sidebar from '../../component/Slidebar/Slidebar';
 
 function Dashboard() {
   return (
     <div className="App">
-      <div className="AppGlass">
-        <Sidebar/>
-        <MainDash/>
-      
+      <div className="AppGlass">  
+        <div className='Sidebar_div'>
+          <div >  <Sidebar/></div>
+        </div>
+        <div className='MainDash_div'>  <MainDash/></div>
       </div>
     </div>
   );
